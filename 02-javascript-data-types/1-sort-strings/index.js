c66134682e00ebe7c119d4670e7b175133de9826
Paul <skrimorill@mail.ru>
2023-01-17 18:32:45 +0300

    1st and 2nd tasks

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,17 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  switch (param) {
+    case 'asc':
+      return makeSorting(arr, 1);
+    case 'desc':
+      return makeSorting(arr, -1);
+    default:
+      return arr;
+    }
+  
+    function makeSorting(array, direction) {
+      return [...array].sort((string1, string2) =>
+        direction * string1.localeCompare(string2, ['ru', 'en'], {caseFirst: 'upper'}));
+    }
 }
