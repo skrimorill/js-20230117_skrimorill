dd1a86040501a30d427d461773a684df621a7bb8
Paul <skrimorill@mail.ru>
2023-02-09 16:26:25 +0300

    06-events-practice/2-tooltip

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,69 @@
 class Tooltip {
-  initialize () {
 
+  static instance
+
+  constructor() {
+    if (!Tooltip.instance) {
+      Tooltip.instance = this
+    }
+    return Tooltip.instance
+  }
+
+  initialize () {
+    document.addEventListener("pointerover", this.pointerOver)    
+    document.addEventListener("pointerout", this.pointerOut)  
+  }
+
+  
+  remove() {
+    if (this._element) {
+        this.element.remove()
+    }
+  }
+
+  destroy() {
+    this.remove()
+    this._element = null
+    document.removeEventListener("pointerover", this.pointerOver)    
+    document.removeEventListener("pointerout", this.pointerOut)   
+    document.removeEventListener("pointermove", this.pointerMove)   
+  }
+
+  render(label) {
+    this.element.innerHTML = label
+    document.body.append(this.element)    
+  }
+
+  pointerOver = (event) => {
+    const tooltipLabel = event.target?.dataset?.tooltip
+
+    if (tooltipLabel) {
+      this.render(tooltipLabel)
+      this.tooltipIsShow = true
+      document.addEventListener("pointermove", this.pointerMove)  
+    }
+  }
+
+  pointerOut = (event) => {
+    if (this.tooltipIsShow) {
+      this.remove()
+      this.tooltipIsShow = false
+      document.removeEventListener("pointermove", this.pointerMove)  
+    }
+  }
+
+  pointerMove = (event) => {
+    this.element.style.left = event.pageX + 10 + 'px';
+    this.element.style.top =  event.pageY + 10 + 'px';
+  }
+
+  get element() {
+    if (!this._element) {
+      this._element = document.createElement("div")
+      this._element.className = "tooltip"
+    }
+
+    return this._element
   }
 }
 
